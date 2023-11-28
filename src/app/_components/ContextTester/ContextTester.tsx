c7a766621bf024cd5d1516ca94b49009ed125542
LEtMeDEv <79841@naver.com>
2023-11-28 23:31:32 +0900

    update context tester

--- src/app/_components/ContextTester/ContextTester.tsx
+++ src/app/_components/ContextTester/ContextTester.tsx
@@ -1,3 +1,46 @@
+"use client";
+import {
+  SelectedItemContextProvider,
+  useSelectedItemContext,
+  useSelectedItemDispatchContext,
+} from "@/context/SelectedItemContext";
+import { items } from "@/data/item";
+import { TItem } from "@/types/Item";
+import { ChangeEventHandler } from "react";
+
+type TContextTesterItemProps = {
+  item: TItem;
+};
+
+function ContextTesterItem({ item }: TContextTesterItemProps) {
+  const selectedItem = useSelectedItemContext();
+  const { selectItem, unselectItem } = useSelectedItemDispatchContext();
+
+  const handleCheck: ChangeEventHandler<HTMLInputElement> = (e) => {
+    if (e.target.checked) {
+      selectItem(item);
+    } else {
+      unselectItem();
+    }
+  };
+  const isChecked =
+    selectedItem != null ? selectedItem.name === item.name : false;
+  return (
+    <div>
+      <input type="checkbox" checked={isChecked} onChange={handleCheck} />
+      <span>{item.name}</span>
+    </div>
+  );
+}
+
 export function ContextTester() {
-  return <div>Enter</div>;
+  return (
+    <SelectedItemContextProvider>
+      <div>
+        {items.map((item: TItem) => (
+          <ContextTesterItem key={item.name} item={item} />
+        ))}
+      </div>
+    </SelectedItemContextProvider>
+  );
 }
