5d46f106c37a50157c2a2096a771b2b618a0e96f
LEtMeDEv <79841@naver.com>
2023-11-28 23:31:39 +0900

    add data

---
+++ src/data/item.ts
@@ -0,0 +1,9 @@
+import { TItem } from "@/types/Item";
+
+export const items: TItem[] = [
+  { name: "keyboard" },
+  { name: "laptop" },
+  { name: "ipad" },
+  { name: "mouse" },
+  { name: "monitor" },
+];
